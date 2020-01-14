521063e4a9b7ce4cfa355bcf71bc76857f78ceb4
Joris ter Wolbeek <21074092@talnet.nl>
2020-01-14 12:18:04 +0100

    Uitwerking van de exercise

---
+++ multiplier.php
@@ -0,0 +1,6 @@
+<?php
+echo("ik ga de een keer som doen met de getallen die jij mij geeft.".PHP_EOL);
+$getal1=readline("het eerste getal?");
+$getal2=readline("het tweede getal?");
+$uitkomst=$getal1*$getal2;
+echo($uitkomst);
